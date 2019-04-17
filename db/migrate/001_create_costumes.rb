8ebaeebbbd981dbed1c41a9fcb59159dd0d890a8
Eric Storey <estorey@gmail.com>
2019-04-17 20:42:08 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,5 +1,10 @@
 class CreateCostumes < ActiveRecord::Migration[4.2]
-  
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :breed
+    end
+  end
 end
 
 # Create a class and inherit from ActiveRecord::Migration
