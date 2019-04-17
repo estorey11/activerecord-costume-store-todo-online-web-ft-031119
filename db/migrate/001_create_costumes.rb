4f63c4ed33461f719754de124a3d740975cb1aa0
Eric Storey <estorey@gmail.com>
2019-04-17 20:40:54 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,3 +1,7 @@
+class CreateCostumes < ActiveRecord::Migration[4.2]
+  
+end
+
 # Create a class and inherit from ActiveRecord::Migration
 
 # By convention, the class name should match the part of the
