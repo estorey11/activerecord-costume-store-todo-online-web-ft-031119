705efce2a45eab0fa9c785a0ef269970f7e443a8
Eric Storey <estorey@gmail.com>
2019-04-17 20:58:03 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,8 +3,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      t.integer :costume_inventory
-      t.integer :num_of_employees
+      t.string :theme
+      t.string :price
       t.boolean :still_in_business
       t.datetime :opening_time
       t.datetime :closing_time
